cd07a26f6653ad70d8d5a7a5493e271b022fbaaa
Richard Ayala <ayalafunes99@outlook.com>
2021-11-08 22:37:27 -0500

    v.0.1.7

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tesina-project'));
+app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist/tesina-project'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + 'dist/tesina-project/index.html'));
 });
