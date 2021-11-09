399201415483781d1210cc44125e766b6243fe34
Richard Ayala <ayalafunes99@outlook.com>
2021-11-08 22:49:35 -0500

    v.0.1.8

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.static(__dirname + '/dist'));
 // app.use(express.static(__dirname + '/dist/tesina-project'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + 'dist/tesina-project/index.html'));
+    res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
